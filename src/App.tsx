779814466c469008d61c501a0aa5e74c92f53ceb
Robert Wendt <robertwendt@Roberts-MacBook-Air.local>
2022-06-04 18:14:16 -0700

    first draft of mergejson form. does not properly handle the list case

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useReducer } from "react";
+import "./App.css";
+
+function handleJsonState(value: any) {
+  try {
+    return JSON.parse(value);
+  } catch (e) {
+    return {};
+  }
+}
+
+function reducer(state: any, action: (state: any) => any) {
+  return action(state);
+}
+
+function MergeJsonForm() {
+  const [state, dispatch] = useReducer(reducer, {
+    count: 2,
+  });
+  return (
+    <>
+      <pre>
+        {JSON.stringify(
+          Array.from(Array(state.count).keys()).reduce(
+            (acc: any, currentVal: any) => ({
+              ...acc,
+              ...handleJsonState(state[currentVal]),
+            }),
+            {}
+          )
+        )}
+      </pre>
+      <div>
+        {Array.from(Array(state.count).keys()).map((idx) => (
+          <textarea
+            onChange={(event) =>
+              dispatch((state) => {
+                return { ...state, [idx]: event.target.value };
+              })
+            }
+            value={idx in state ? state[idx] : ""}
+          ></textarea>
+        ))}
+      </div>
+      <button onClick={() => dispatch((_) => ({ count: 2 }))}>reset</button>
+
+      <button
+        onClick={() =>
+          dispatch((state) => ({ ...state, count: Math.max(state.count - 1) }))
+        }
+      >
+        -
+      </button>
+      <button
+        onClick={() =>
+          dispatch((state) => ({ ...state, count: state.count + 1 }))
+        }
+      >
+        +
+      </button>
+    </>
+  );
+}
 
 function App() {
+  // Declare a new state variable, which we'll call "count"
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MergeJsonForm />
     </div>
   );
 }
