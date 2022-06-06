44aadf2af5c2979d45cbe376cef19bf40836d080
Robert Wendt <robertwendt@Roberts-MacBook-Air.local>
2022-06-06 13:21:12 -0700

    move lambda to named function, add todo with refactor idea

--- src/App.tsx
+++ src/App.tsx
@@ -13,32 +13,35 @@ function reducer(state: any, action: (state: any) => any) {
   return action(state);
 }
 
+// TODO: this is a little annoying because it relies on passing in state to 
+// create the reducer function. for the list of state id's, should 
+// map handleJsonState(state[currentVal]) then reduce to merge json.
+const mergeJsonNaiveREducer = (state: any) => (acc: any, currentVal: any) => ({
+  ...acc,
+  ...handleJsonState(state[currentVal]),
+});
+
 function MergeJson() {
   const [state, dispatch] = useReducer(reducer, {
     count: 2,
   });
+
+  const stateKeys = Array.from(Array(state.count).keys());
+
   return (
     <>
       <pre>
         {JSON.stringify(
-          Array.from(Array(state.count).keys()).reduce(
-            (acc: any, currentVal: any) => ({
-              ...acc,
-              ...handleJsonState(state[currentVal]),
-            }),
-            {}
-          ),
+          stateKeys.reduce(mergeJsonNaiveREducer(state), {}),
           null,
           "  "
         )}
       </pre>
       <div>
-        {Array.from(Array(state.count).keys()).map((idx) => (
+        {stateKeys.map((idx) => (
           <textarea
             onChange={(event) =>
-              dispatch((state) => {
-                return { ...state, [idx]: event.target.value };
-              })
+              dispatch((state) => ({ ...state, [idx]: event.target.value }))
             }
             value={idx in state ? state[idx] : ""}
           ></textarea>
@@ -48,7 +51,10 @@ function MergeJson() {
 
       <button
         onClick={() =>
-          dispatch((state) => ({ ...state, count: Math.max(state.count - 1, 0) }))
+          dispatch((state) => ({
+            ...state,
+            count: Math.max(state.count - 1, 0),
+          }))
         }
       >
         -
@@ -65,7 +71,6 @@ function MergeJson() {
 }
 
 function App() {
-  // Declare a new state variable, which we'll call "count"
   return (
     <div className="App">
       <MergeJson />
